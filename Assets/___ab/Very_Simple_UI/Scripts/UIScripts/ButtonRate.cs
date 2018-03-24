c3c503d263cbf8dd1cecc376843e274c8f055e2d
Badri Narayan Mohan <badrinarayanmohan@gmail.com>
2018-03-24 08:23:28 +0530

    Initial Commit

---
+++ Assets/___ab/Very_Simple_UI/Scripts/UIScripts/ButtonRate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if AADOTWEEN
+namespace AppAdvisory.UI
+{
+	/// <summary>
+	/// Attached to rate button
+	/// </summary>
+	public class ButtonRate : MonoBehaviour 
+	{
+				/// <summary>
+		/// If player clicks on the rate button, we call this method.
+		/// </summary>
+		public void OnClickedRate()
+		{
+			Application.OpenURL(FindObjectOfType<UIController>().URL_STORE);
+		}
+	}
+}
+#endif
